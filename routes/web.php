89629c76655cd1f4a26753d9154378ca06ba4c30
yanisparra1 <yanisparra1@gmail.com>
2025-12-31 00:34:48 +0000

    prueba

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,5 @@ use App\Http\Controllers\HomeController;
 Route::get('/', function () {
     return view('welcome');
 });
-
+//prueba
 Route::get('/create-pdf/{historia}', [HomeController::class, 'createPDF'])->name('createPDF');
